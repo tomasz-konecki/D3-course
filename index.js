d67cb74ade80d61582766b8f0d4761109068f655
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2018-05-11 14:09:21 +0200

    Added code

--- index.js
+++ index.js
@@ -1,44 +1,85 @@
-var data = [
-    {"platform": "Android", "percentage": 43.25},
-    {"platform": "Windows", "percentage": 36.69},
-    {"platform": "iOS", "percentage": 20.06}
-];
+//API to fetch historical data of Bitcoin Price Index
+const api = 'https://api.coindesk.com/v1/bpi/historical/close.json?start=2017-12-31&end=2018-04-01';
 
-var svgWidth = 500, svgHeight = 300, radius =  Math.min(svgWidth, svgHeight) / 2;
+/**
+ * Loading data from API when DOM Content has been loaded'.
+ */
+document.addEventListener("DOMContentLoaded", function(event) {
+fetch(api)
+    .then(function(response) { return response.json(); })
+    .then(function(data) {
+        var parsedData = parseData(data);
+        drawChart(parsedData);
+    })
+    .catch(function(err) { console.log(err); })
+});
+
+/**
+ * Parse data into key-value pairs
+ * @param {object} data Object containing historical data of BPI
+ */
+function parseData(data) {
+    var arr = [];
+    for (var i in data.bpi) {
+        arr.push({
+            date: new Date(i), //date
+            value: +data.bpi[i] //convert string to number
+        });
+    }
+    return arr;
+}
+
+/**
+ * Creates a chart using D3
+ * @param {object} data Object containing historical data of BPI
+ */
+function drawChart(data) {
+var svgWidth = 600, svgHeight = 400;
+var margin = { top: 20, right: 20, bottom: 30, left: 50 };
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
 var svg = d3.select('svg')
     .attr("width", svgWidth)
     .attr("height", svgHeight);
 
-//Create group element to hold pie chart
 var g = svg.append("g")
-    .attr("transform", "translate(" + radius + "," + radius + ")") ;
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var color = d3.scaleOrdinal(d3.schemeCategory10);
+var x = d3.scaleTime()
+    .rangeRound([0, width]);
 
-var pie = d3.pie().value(function(d) {
-     return d.percentage;
-});
+var y = d3.scaleLinear()
+    .rangeRound([height, 0]);
 
-var path = d3.arc()
-    .outerRadius(radius)
-    .innerRadius(0);
+var line = d3.line()
+    .x(function(d) { return x(d.date)})
+    .y(function(d) { return y(d.value)})
+    x.domain(d3.extent(data, function(d) { return d.date }));
+    y.domain(d3.extent(data, function(d) { return d.value }));
 
-var arc = g.selectAll("arc")
-    .data(pie(data))
-    .enter()
-    .append("g");
+g.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .select(".domain")
+    .remove();
 
-arc.append("path")
-    .attr("d", path)
-    .attr("fill", function(d) { return color(d.data.percentage); });
+g.append("g")
+    .call(d3.axisLeft(y))
+    .append("text")
+    .attr("fill", "#000")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", "0.71em")
+    .attr("text-anchor", "end")
+    .text("Price ($)");
 
-var label = d3.arc()
-    .outerRadius(radius)
-    .innerRadius(0);
-
-arc.append("text")
-    .attr("transform", function(d) {
-        return "translate(" + label.centroid(d) + ")";
-    })
-    .attr("text-anchor", "middle")
-    .text(function(d) { return d.data.platform+":"+d.data.percentage+"%"; });
+g.append("path")
+    .datum(data)
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-linejoin", "round")
+    .attr("stroke-linecap", "round")
+    .attr("stroke-width", 1.5)
+    .attr("d", line);
+}
