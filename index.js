d65527bc04b24e683ef3ab67a035480e723d3c46
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2018-05-11 12:55:13 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,57 +1,31 @@
-//var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
-var dataset = [1,2,3,4,5,6,7];
+var data= [80, 100, 56, 120, 180, 30, 40, 120, 160];
 
-var svgWidth = 500, svgHeight = 300, barPadding = 5;
-var barWidth = (svgWidth / dataset.length);
-
+var svgWidth = 600, svgHeight = 300;
 
 var svg = d3.select('svg')
     .attr("width", svgWidth)
     .attr("height", svgHeight);
 
-var barChart = svg.selectAll("rect")
-    .data(dataset)
-    .enter()
-    .append("rect")
-    .attr("y", function(d) {
-         return svgHeight - d
-    })
-    .attr("height", function(d) {
-        return d;
-    })
-    .attr("width", barWidth - barPadding)
-    .attr("transform", function (d, i) {
-        var translate = [barWidth * i, 0];
-        return "translate("+ translate +")";
-    });
-
-
-// after scaling
-
-var svgWidth = 500, svgHeight = 300, barPadding = 5;
-var barWidth = (svgWidth / dataset.length);
-
-
-var svg = d3.select('.bar__chart-scale')
-   .attr("width", svgWidth)
-   .attr("height", svgHeight);
+var xScale = d3.scaleLinear()
+    .domain([0, d3.max(data) ])
+    .range([0, svgWidth]);
 
 var yScale = d3.scaleLinear()
-   .domain([0, d3.max(dataset)])
-   .range([0, svgHeight]);
+    .domain([0, d3.max(data)])
+    .range([svgHeight, 0]);
 
-var barChart = svg.selectAll("rect")
-   .data(dataset)
-   .enter()
-   .append("rect")
-   .attr("y", function(d) {
-        return svgHeight - yScale(d)
-   })
-   .attr("height", function(d) {
-       return yScale(d);
-   })
-   .attr("width", barWidth - barPadding)
-   .attr("transform", function (d, i) {
-       var translate = [barWidth * i, 0];
-       return "translate("+ translate +")";
-   });
+var x_axis = d3.axisBottom()
+    .scale(xScale);
+
+var y_axis = d3.axisLeft()
+    .scale(yScale);
+
+svg.append("g")
+    .attr("transform", "translate(50, -20)")
+    .call(y_axis);
+
+var xAxisTranslate = svgHeight - 20;
+
+svg.append("g")
+    .attr("transform", "translate(50, " + xAxisTranslate  +")")
+    .call(x_axis);
