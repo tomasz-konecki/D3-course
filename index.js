7718db0daf9a5cddc03c39a49f130e3588410200
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2018-05-11 13:34:00 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
-var svgWidth = 600,
-    svgHeight = 300;
+var data = [
+    {"platform": "Android", "percentage": 40.11},
+    {"platform": "Windows", "percentage": 36.69},
+    {"platform": "iOS", "percentage": 13.06}
+];
 
-var svg = d3.select("svg")
+var svgWidth = 500, svgHeight = 300, radius =  Math.min(svgWidth, svgHeight) / 2;
+var svg = d3.select('svg')
     .attr("width", svgWidth)
-    .attr("height", svgHeight)
-    .attr("class", "svg-container");
+    .attr("height", svgHeight);
 
-var line = svg.append("line")
-    .attr("x1", 10)
-    .attr("x2", 500)
-    .attr("y1", 50)
-    .attr("y2", 50)
-    .attr("stroke", "red")
+//Create group element to hold pie chart
+var g = svg.append("g")
+    .attr("transform", "translate(" + radius + "," + radius + ")") ;
 
-var rect = svg.append("rect")
-    .attr("x", 100)
-    .attr("y", 100)
-    .attr("width", 200)
-    .attr("height", 100)
-    .attr("fill", "#9B95FF")
+var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+var pie = d3.pie().value(function(d) {
+     return d.percentage;
+});
+
+var path = d3.arc()
+    .outerRadius(radius)
+    .innerRadius(0);
+
+var arc = g.selectAll("arc")
+    .data(pie(data))
+    .enter()
+    .append("g");
+
+arc.append("path")
+    .attr("d", path)
+    .attr("fill", function(d) { return color(d.data.percentage); });
+
+var label = d3.arc()
+    .outerRadius(radius)
+    .innerRadius(0);
+
+arc.append("text")
+    .attr("transform", function(d) {
+        return "translate(" + label.centroid(d) + ")";
+    })
+    .attr("text-anchor", "middle")
+    .text(function(d) { return d.data.platform+":"+d.data.percentage+"%"; });
