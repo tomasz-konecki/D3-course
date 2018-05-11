f2af87ed1ce20ae7ebf39bc6f07e2e5081e1fa08
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2018-05-11 13:28:14 +0200

    Added code

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
-var data= [80, 100, 56, 120, 180, 30, 40, 120, 160];
+var svgWidth = 600,
+    svgHeight = 300;
 
-var svgWidth = 600, svgHeight = 300;
-
-var svg = d3.select('svg')
+var svg = d3.select("svg")
     .attr("width", svgWidth)
-    .attr("height", svgHeight);
+    .attr("height", svgHeight)
+    .attr("class", "svg-container");
 
-var xScale = d3.scaleLinear()
-    .domain([0, d3.max(data) ])
-    .range([0, svgWidth]);
+var line = svg.append("line")
+    .attr("x1", 10)
+    .attr("x2", 500)
+    .attr("y1", 50)
+    .attr("y2", 50)
+    .attr("stroke", "red")
 
-var yScale = d3.scaleLinear()
-    .domain([0, d3.max(data)])
-    .range([svgHeight, 0]);
-
-var x_axis = d3.axisBottom()
-    .scale(xScale);
-
-var y_axis = d3.axisLeft()
-    .scale(yScale);
-
-svg.append("g")
-    .attr("transform", "translate(50, -20)")
-    .call(y_axis);
-
-var xAxisTranslate = svgHeight - 20;
-
-svg.append("g")
-    .attr("transform", "translate(50, " + xAxisTranslate  +")")
-    .call(x_axis);
+var rect = svg.append("rect")
+    .attr("x", 100)
+    .attr("y", 100)
+    .attr("width", 200)
+    .attr("height", 100)
+    .attr("fill", "#9B95FF")
